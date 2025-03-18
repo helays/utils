efabdc52725854e438926ba6fc39c7446a2fb996
helei <helei@itestor.com>
2025-03-18 09:29:32 +0800

    格式化引擎 功能优化，支持泛型
    Signed-off-by: helei <helei@itestor.com>

--- formatRuleEngine/format.rule.date.go
+++ formatRuleEngine/format.rule.date.go
@@ -1,20 +1,26 @@
 package formatRuleEngine
 
 import (
+	"fmt"
 	"github.com/araddon/dateparse"
 	"github.com/helays/utils/tools"
 	"time"
 )
 
 // 时间格式化
-func (this FormatRule) dateFormat(_src any) (string, error) {
+func (this FormatRule[T]) dateFormat(_src any) (any, error) {
 	var (
 		t   time.Time
 		err error
-		src = tools.Any2string(_src)
+		ok  bool
+		src string
 	)
+	if t, ok = _src.(time.Time); !ok {
+		src = tools.Any2string(_src)
+		t, err = dateparse.ParseLocal(src)
+	}
 	// 首先尝试使用 https://github.com/araddon/dateparse 库
-	t, err = dateparse.ParseLocal(src)
+
 	if err == nil {
 		if this.OutputRule != "" {
 			return t.Format(this.OutputRule), nil
@@ -36,4 +42,33 @@ func (this FormatRule) dateFormat(_src any) (string, error) {
 		return t.Format(time.DateTime), err
 	}
 	return src, nil
+}
+
+// 时间格式化
+func (this FormatRule[T]) dateObjectFormat(_src any) (any, error) {
+	var (
+		t   time.Time
+		err error
+		ok  bool
+		src = tools.Any2string(_src)
+	)
+	// 首先尝试使用 https://github.com/araddon/dateparse 库
+	if t, ok = _src.(time.Time); !ok {
+		t, err = dateparse.ParseLocal(src)
+	}
+	if err == nil {
+		return t, nil
+	}
+	for _, format := range this.InputRules {
+		if format == "timestamp" {
+			t, err = tools.AutoDetectTimestampString(src)
+		} else {
+			t, err = time.Parse(format, src)
+		}
+		if err != nil {
+			continue
+		}
+		return t, nil
+	}
+	return t, fmt.Errorf("时间解析失败：%s", _src)
 }
--- formatRuleEngine/format.rule.go
+++ formatRuleEngine/format.rule.go
@@ -1,16 +1,30 @@
 package formatRuleEngine
 
-type FormatRule struct {
+import "fmt"
+
+type FormatRule[T any] struct {
 	FormatType string   `yaml:"format_type" json:"format_type"` // 格式化类型
 	InputRules []string `yaml:"input_rules" json:"input_rules"` // 识别格式化规则
 	OutputRule string   `yaml:"output_rule" json:"output_rule"` //  输出格式规则
 }
 
 // Format 格式化
-func (this FormatRule) Format(src any) (any, error) {
+func (this FormatRule[T]) Format(src any) (T, error) {
+	var (
+		zero   T // 创建一个 T 类型的零值
+		result any
+		err    error
+	)
 	switch this.FormatType {
 	case "date_format":
-		return this.dateFormat(src)
+		result, err = this.dateFormat(src)
+	case "output_date":
+		result, err = this.dateObjectFormat(src)
+	default:
+		return zero, fmt.Errorf("不支持的格式化类型：%s", this.FormatType)
 	}
-	return src, nil
+	if err != nil {
+		return zero, err
+	}
+	return result.(T), nil
 }
