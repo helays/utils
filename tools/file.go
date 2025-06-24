452f005ea05df40dd7047cecd341bb2ecf3fec43
helei <helei@itestor.com>
2025-06-24 16:01:06 +0800

    ReadRowWithFile 支持最大10M的缓冲区，只要一行数据不超过10M 理论上都可以按行读取
    Signed-off-by: helei <helei@itestor.com>

--- tools/file.go
+++ tools/file.go
@@ -10,6 +10,8 @@ type ReadRowCallback func(scanner *bufio.Scanner) error
 
 func ReadRowWithFile(file *os.File, callback ReadRowCallback) error {
 	scanner := bufio.NewScanner(file)
+	// 初始 4KB，最大 10MB（可根据需求调整）
+	scanner.Buffer(make([]byte, 4096), 10*1024*1024)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		if err := callback(scanner); err != nil {
