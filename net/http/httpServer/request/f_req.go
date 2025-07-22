353b7c4173ab374a990450935a72e632875607d1
helei <helei@itestor.com>
2025-07-22 17:12:15 +0800

    新增一个GetHeaderValueFormatInt通用函数
    Signed-off-by: helei <helei@itestor.com>

--- net/http/httpServer/request/f_req.go
+++ net/http/httpServer/request/f_req.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"fmt"
+	"github.com/helays/utils/v2/tools"
 	"net"
 	"net/http"
 )
@@ -21,4 +23,18 @@ func Getip(r *http.Request) string {
 		remoteAddr = "127.0.0.1"
 	}
 	return remoteAddr
+}
+
+// GetHeaderValueFormatInt 获取多个header字段，并转为int
+func GetHeaderValueFormatInt(header http.Header, fields ...string) (map[string]int, error) {
+	resp := make(map[string]int)
+	for _, field := range fields {
+		_v := header.Get(field)
+		v, err := tools.Any2int(_v)
+		if err != nil {
+			return nil, fmt.Errorf("不合法的字段[%s]值 %s %v", field, _v, err)
+		}
+		resp[field] = int(v)
+	}
+	return resp, nil
 }
