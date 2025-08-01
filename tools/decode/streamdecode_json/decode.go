4ae8069f7b775ec2d8d8363bef11a9fdf79c533e
helei <helei@itestor.com>
2025-08-01 19:00:36 +0800

    新增一个流式解析json 的通用函数
    Signed-off-by: helei <helei@itestor.com>

--- tools/decode/streamdecode_json/decode.go
+++ tools/decode/streamdecode_json/decode.go
@@ -2,6 +2,7 @@ package streamdecode_json
 
 import (
 	"context"
+	"fmt"
 	"github.com/goccy/go-json"
 	"github.com/helays/utils/v2/dataType/customWriter"
 	"io"
@@ -13,8 +14,8 @@ func ProcessJSON(ctx context.Context, r io.Reader, handler JSONHandler) (totalSi
 	// 创建计数reader
 	counter := &customWriter.SizeCounter{}
 	teeReader := io.TeeReader(r, counter)
-
 	decoder := json.NewDecoder(teeReader)
+
 	// 读取第一个token判断是数组还是对象
 	t, err := decoder.Token()
 	if err != nil {
@@ -24,10 +25,24 @@ func ProcessJSON(ctx context.Context, r io.Reader, handler JSONHandler) (totalSi
 	switch delim := t.(type) {
 	case json.Delim:
 		switch delim {
-		case '{':
-			var obj map[string]any
-			if err = decoder.Decode(&obj); err != nil {
-				return counter.TotalSize, err
+		case '{': // 由于提前消费了 { 字符，所以这里需要手动进行解析。
+			obj := make(map[string]any)
+			for decoder.More() {
+				// 读取 key
+				key, _err := decoder.Token()
+				if _err != nil {
+					return counter.TotalSize, err
+				}
+				keyStr, ok := key.(string)
+				if !ok {
+					return counter.TotalSize, fmt.Errorf("expected string key, got %T", key)
+				}
+				// 读取 value
+				var val any
+				if err = decoder.Decode(&val); err != nil {
+					return counter.TotalSize, err
+				}
+				obj[keyStr] = val
 			}
 			if err = handler(ctx, obj); err != nil {
 				return counter.TotalSize, err
