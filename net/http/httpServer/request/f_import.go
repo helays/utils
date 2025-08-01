c093201a5edf2ee2c18e11a089ad361119e9e967
helei <helei@itestor.com>
2025-08-01 18:18:06 +0800

    新增一个流式解析json 的通用函数
    Signed-off-by: helei <helei@itestor.com>

--- net/http/httpServer/request/f_import.go
+++ net/http/httpServer/request/f_import.go
@@ -2,6 +2,7 @@ package request
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"github.com/helays/utils/v2/dataType/customWriter"
 	"github.com/helays/utils/v2/excelTools"
@@ -19,21 +20,33 @@ type Import struct {
 	Sep      string `json:"sep"`       // csv 分割符
 }
 
-func (this Import) Import(r *http.Request) ([]any, int64, error) {
-	switch this.FileType {
+type JSONHandler func(ctx context.Context, obj map[string]interface{}) error
+
+func (i *Import) Import(r *http.Request) ([]any, int64, error) {
+	switch i.FileType {
 	case "excel":
-		return this.ImportExcel(r)
+		return i.ImportExcel(r)
 	case "csv":
-		return this.ImportCsv(r)
+		return i.ImportCsv(r)
 	default:
-		return nil, 0, fmt.Errorf("不支持的文件类型：%s", this.FileType)
+		return nil, 0, fmt.Errorf("不支持的文件类型：%s", i.FileType)
 	}
 }
 
-// ImportExcel 获取excel内容
-func (this Import) ImportExcel(r *http.Request) ([]any, int64, error) {
-	if err := this.valid(); err != nil {
-		return nil, 0, err
+func (i *Import) ImportWithHandler(r *http.Request, handler JSONHandler) (int64, error) {
+	switch i.FileType {
+	case "excel":
+		return i.ImportExcelWithHandler(r, handler)
+	case "csv":
+		return i.ImportCsvWithHandler(r, handler)
+	default:
+		return 0, fmt.Errorf("不支持的文件类型：%s", i.FileType)
+	}
+}
+
+func (i *Import) ImportExcelWithHandler(r *http.Request, handler JSONHandler) (int64, error) {
+	if err := i.valid(); err != nil {
+		return 0, err
 	}
 	counter := &customWriter.SizeCounter{}
 	teeReader := io.TeeReader(r.Body, counter)
@@ -41,33 +54,35 @@ func (this Import) ImportExcel(r *http.Request) ([]any, int64, error) {
 	excel, err := excelize.OpenReader(teeReader)
 	defer excelTools.CloseExcel(excel)
 	if err != nil {
-		return nil, counter.TotalSize, fmt.Errorf("excel文件打开失败：%s", err.Error())
+		return counter.TotalSize, fmt.Errorf("excel文件打开失败：%s", err.Error())
 	}
 	rows, err := excel.GetRows(excel.GetSheetName(0))
 	if err != nil {
-		return nil, counter.TotalSize, fmt.Errorf("sheet读取失败：%s", err.Error())
+		return counter.TotalSize, fmt.Errorf("sheet读取失败：%s", err.Error())
 	}
-	if len(rows) < this.DataRow {
-		return nil, counter.TotalSize, fmt.Errorf("未读取到有效数据")
+	if len(rows) < i.DataRow {
+		return counter.TotalSize, fmt.Errorf("未读取到有效数据")
 	}
 	var (
-		data        []any
-		dataRow     = this.DataRow - 1
-		fieldRowMap = rows[this.FieldRow-1]
+		dataRow     = i.DataRow - 1
+		fieldRowMap = rows[i.FieldRow-1]
 	)
 
 	for idx, row := range rows {
 		if idx < dataRow {
 			continue
 		}
-		data = append(data, tools.Slice2MapWithHeader(row, fieldRowMap))
+		err = handler(r.Context(), tools.Slice2MapWithHeader(row, fieldRowMap))
+		if err != nil {
+			return counter.TotalSize, err
+		}
 	}
-	return data, counter.TotalSize, nil
+	return counter.TotalSize, nil
 }
 
-func (this Import) ImportCsv(r *http.Request) ([]any, int64, error) {
-	if err := this.valid(); err != nil {
-		return nil, 0, err
+func (i *Import) ImportCsvWithHandler(r *http.Request, handler JSONHandler) (int64, error) {
+	if err := i.valid(); err != nil {
+		return 0, err
 	}
 	counter := &customWriter.SizeCounter{}
 	teeReader := io.TeeReader(r.Body, counter)
@@ -75,28 +90,50 @@ func (this Import) ImportCsv(r *http.Request) ([]any, int64, error) {
 		scanner   = bufio.NewScanner(teeReader)
 		idx       int
 		fieldRows []string
-		data      []any
 	)
-	this.Sep = tools.Ternary(this.Sep == "", ",", this.Sep)
+	i.Sep = tools.Ternary(i.Sep == "", ",", i.Sep)
 	for scanner.Scan() {
 		idx++
 		line := scanner.Text()
-		lineRows := strings.Split(line, this.Sep)
-		if idx == this.FieldRow {
+		lineRows := strings.Split(line, i.Sep)
+		if idx == i.FieldRow {
 			fieldRows = lineRows
 			continue
 		}
-		if idx < this.DataRow {
+		if idx < i.DataRow {
 			continue
 		}
-		data = append(data, tools.Slice2MapWithHeader(lineRows, fieldRows))
+		if err := handler(r.Context(), tools.Slice2MapWithHeader(lineRows, fieldRows)); err != nil {
+			return 0, err
+		}
 	}
-	return data, counter.TotalSize, nil
+	return counter.TotalSize, nil
+}
+
+// ImportExcel 获取excel内容
+func (i *Import) ImportExcel(r *http.Request) ([]any, int64, error) {
+	var data []any
+	flow, err := i.ImportExcelWithHandler(r, func(ctx context.Context, obj map[string]interface{}) error {
+		data = append(data, obj)
+		return nil
+	})
+	return data, flow, err
+
+}
+
+// ImportCsv 获取csv内容
+func (i *Import) ImportCsv(r *http.Request) ([]any, int64, error) {
+	var data []any
+	flow, err := i.ImportCsvWithHandler(r, func(ctx context.Context, obj map[string]interface{}) error {
+		data = append(data, obj)
+		return nil
+	})
+	return data, flow, err
 }
 
 // 参数验证
-func (this *Import) valid() error {
-	if this.FieldRow == 0 || this.FieldRow >= this.DataRow {
+func (i *Import) valid() error {
+	if i.FieldRow == 0 || i.FieldRow >= i.DataRow {
 		return fmt.Errorf("无有效字段、数据所在的行数")
 	}
 	return nil
