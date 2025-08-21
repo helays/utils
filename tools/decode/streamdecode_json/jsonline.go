9a78f0eded34420bed89cc4b0a39240f3a1e364e
helei <helei@itestor.com>
2025-08-21 11:34:45 +0800

    大行模式优化。
    Signed-off-by: helei <helei@itestor.com>

--- tools/decode/streamdecode_json/jsonline.go
+++ tools/decode/streamdecode_json/jsonline.go
@@ -3,8 +3,9 @@ package streamdecode_json
 import (
 	"bufio"
 	"encoding/json"
-	"github.com/helays/utils/v2/dataType/customWriter"
 	"io"
+
+	"github.com/helays/utils/v2/dataType/customWriter"
 )
 
 func (i *Import) processLine(handler JSONHandler) (totalSize int64, err error) {
@@ -15,7 +16,7 @@ func (i *Import) processLine(handler JSONHandler) (totalSize int64, err error) {
 	for scanner.Scan() {
 		select {
 		case <-i.ctx.Done():
-			return 0, i.ctx.Err()
+			return counter.TotalSize, i.ctx.Err()
 		default:
 			line := scanner.Text()
 			var dst map[string]any
@@ -28,4 +29,61 @@ func (i *Import) processLine(handler JSONHandler) (totalSize int64, err error) {
 		}
 	}
 	return counter.TotalSize, nil
+}
+
+// processLineEnhanced 优化处理超长行
+func (i *Import) processLineEnhanced(handler JSONHandler) (totalSize int64, err error) {
+	counter := &customWriter.SizeCounter{}
+	teeReader := io.TeeReader(i.rd, counter)
+	reader := bufio.NewReader(teeReader)
+	for {
+		select {
+		case <-i.ctx.Done():
+			return counter.TotalSize, i.ctx.Err()
+		default:
+			line, _err := i.readFullLine(reader)
+			if _err != nil {
+				if err == io.EOF {
+					return counter.TotalSize, nil
+				}
+				return counter.TotalSize, _err
+			}
+			if len(line) > 0 {
+				var dst map[string]any
+				if err = json.Unmarshal(line, &dst); err != nil {
+					return counter.TotalSize, err
+				}
+				if err = handler(i.ctx, dst); err != nil {
+					return counter.TotalSize, err
+				}
+			}
+		}
+	}
+}
+
+// readFullLine 读取完整的一行（处理超长行）
+func (i *Import) readFullLine(reader *bufio.Reader) ([]byte, error) {
+	var line []byte
+	var isPrefix bool
+	var err error
+
+	for {
+		var partial []byte
+		partial, isPrefix, err = reader.ReadLine()
+		if err != nil {
+			break
+		}
+
+		line = append(line, partial...)
+		if !isPrefix {
+			break
+		}
+	}
+
+	// 处理EOF时可能有部分数据的情况
+	if err == io.EOF && len(line) > 0 {
+		err = nil // 重置错误，让外层处理数据
+	}
+
+	return line, err
 }
--- tools/decode/streamdecode_json/struct.go
+++ tools/decode/streamdecode_json/struct.go
@@ -3,8 +3,9 @@ package streamdecode_json
 import (
 	"context"
 	"fmt"
-	"github.com/helays/utils/v2/dataType"
 	"io"
+
+	"github.com/helays/utils/v2/dataType"
 )
 
 type JSONHandler func(ctx context.Context, obj map[string]interface{}) error
@@ -13,6 +14,7 @@ type Import struct {
 	fileType dataType.ContentType
 	ctx      context.Context
 	rd       io.Reader
+	bigLine  bool   // 是否启用大行模式
 	FieldRow int    `json:"field_row"` // 字段所在行
 	DataRow  int    `json:"data_row"`  // 数据开始行
 	Sep      string `json:"sep"`       // csv 分割符
@@ -38,6 +40,10 @@ func (i *Import) SetSep(sep string) {
 	i.Sep = sep
 }
 
+func (i *Import) SetBigLine(bigLine bool) {
+	i.bigLine = bigLine
+}
+
 func (i *Import) Import() ([]any, int64, error) {
 	var data []any
 	flow, err := i.ImportWithHandler(func(ctx context.Context, obj map[string]interface{}) error {
@@ -50,6 +56,9 @@ func (i *Import) Import() ([]any, int64, error) {
 func (i *Import) ImportWithHandler(handler JSONHandler) (int64, error) {
 	switch i.fileType {
 	case dataType.PlainText:
+		if i.bigLine {
+			return i.processLineEnhanced(handler)
+		}
 		return i.processLine(handler)
 	case dataType.JSON:
 		return i.processJSON(handler)
