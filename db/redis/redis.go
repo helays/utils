4c4d9211aa9d00858c2cba88c2014d40f5b84de5
helei <helei@itestor.com>
2025-11-04 09:52:13 +0800

    新增一点redis 配置。
    Signed-off-by: helei <helei@itestor.com>

--- db/redis/redis.go
+++ db/redis/redis.go
@@ -3,10 +3,12 @@ package redis
 import (
 	"context"
 	"fmt"
+	"time"
+
 	"github.com/helays/utils/v2/logger/ulogs"
 	"github.com/helays/utils/v2/tools"
 	"github.com/redis/go-redis/v9"
-	"time"
+	"github.com/redis/go-redis/v9/maintnotifications"
 )
 
 // 配置说明
@@ -33,65 +35,78 @@ type Rediscfg struct {
 	// 具体来说，当 DisableIndentity 设置为 true 时，它会阻止客户端在建立连接时自动发送命令来设置自己的标识信息。
 	// 这通常涉及到通过 CLIENT SETINFO LIBRARY 或类似的命令向 Redis 服务器报告客户端库的名称和版本等信息。
 	// 在某些情况下，这可能会导致一些问题，例如，当客户端库不支持这些命令时，或者当应用程序需要控制客户端标识信息的设置方式时。
-	DisableIndentity         bool   `json:"disable_identity" yaml:"disable_identity" ini:"disable_identity"`                                     //  是否禁用在连接时设置客户端库标识的行为
-	IdentitySuffix           string `json:"identity_suffix" yaml:"identity_suffix" ini:"identity_suffix"`                                        // 默认为空, 用于在客户端标识信息中添加后缀
-	EnableSetDbBeforeConnect bool   `json:"enable_set_db_before_connect" yaml:"enable_set_db_before_connect" ini:"enable_set_db_before_connect"` // 在连接前启用设置db编号
-	EnableSetDbAfterConnect  bool   `json:"enable_set_db_after_connect" yaml:"enable_set_db_after_connect" ini:"enable_set_db_after_connect"`    // 在连接后启用设置db编号
-	EnableAuthOnConnect      bool   `json:"enable_auth_on_connect" yaml:"enable_auth_on_connect" ini:"enable_auth_on_connect"`                   // 在 自定义连接中使用auth ,跟下面的 OnConnect效果一样
-	OnConnect                bool   `json:"on_connect" yaml:"on_connect" ini:"on_connect"`                                                       // 主要是在云组件ctg cache 的时候，才需要这个，其他情况一般不需要
-	CustomScan               bool   `ini:"custom_scan" yaml:"custom_scan" json:"custom_scan"`                                                    // 系统中使用scan 扫描的时候，云组件可能需要用这个
-	EnableCheckOnInit        bool   `ini:"enable_check_on_init" yaml:"enable_check_on_init" json:"enable_check_on_init"`                         // 是否在初始化的时候启用 ping测试
+	DisableIdentity          bool                      `json:"disable_identity" yaml:"disable_identity" ini:"disable_identity"`                                     //  是否禁用在连接时设置客户端库标识的行为
+	IdentitySuffix           string                    `json:"identity_suffix" yaml:"identity_suffix" ini:"identity_suffix"`                                        // 默认为空, 用于在客户端标识信息中添加后缀
+	EnableSetDbBeforeConnect bool                      `json:"enable_set_db_before_connect" yaml:"enable_set_db_before_connect" ini:"enable_set_db_before_connect"` // 在连接前启用设置db编号
+	EnableSetDbAfterConnect  bool                      `json:"enable_set_db_after_connect" yaml:"enable_set_db_after_connect" ini:"enable_set_db_after_connect"`    // 在连接后启用设置db编号
+	EnableAuthOnConnect      bool                      `json:"enable_auth_on_connect" yaml:"enable_auth_on_connect" ini:"enable_auth_on_connect"`                   // 在 自定义连接中使用auth ,跟下面的 OnConnect效果一样
+	OnConnect                bool                      `json:"on_connect" yaml:"on_connect" ini:"on_connect"`                                                       // 主要是在云组件ctg cache 的时候，才需要这个，其他情况一般不需要
+	CustomScan               bool                      `ini:"custom_scan" yaml:"custom_scan" json:"custom_scan"`                                                    // 系统中使用scan 扫描的时候，云组件可能需要用这个
+	EnableCheckOnInit        bool                      `ini:"enable_check_on_init" yaml:"enable_check_on_init" json:"enable_check_on_init"`                         // 是否在初始化的时候启用 ping测试
+	MaintNotificationsConfig *MaintNotificationsConfig `json:"maint_notifications_config" yaml:"maint_notifications_config" ini:"maint_notifications_config"`       // 提供维护通知升级的配置。
+}
+
+type MaintNotificationsConfig struct {
+	Mode         maintnotifications.Mode         `json:"mode" yaml:"mode" ini:"mode"`                            // 配置维护通知的运行模式。
+	EndpointType maintnotifications.EndpointType `json:"endpoint_type" yaml:"endpoint_type" ini:"endpoint_type"` // 配置维护通知的端点类型。
 }
 
 // NewUniversalClient 创建一个通用的 Redis 客户端
-func (this Rediscfg) NewUniversalClient() (redis.UniversalClient, error) {
+func (r Rediscfg) NewUniversalClient() (redis.UniversalClient, error) {
 	c := redis.UniversalOptions{
-		Addrs:            this.Addrs,
-		ClientName:       this.ClientName,
-		Username:         this.User,
-		Password:         this.Password,
-		SentinelUsername: this.SentinelUsername,
-		SentinelPassword: this.SentinelPassword,
-		MasterName:       this.MasterName,
-		DisableIndentity: this.DisableIndentity,
-		IdentitySuffix:   this.IdentitySuffix,
-		PoolSize:         this.PoolSize,
-		PoolTimeout:      tools.AutoTimeDuration(this.PoolTimeout, time.Second),
-		MinIdleConns:     this.MinIdleConns,
-		MaxIdleConns:     this.MaxIdleConns,
-		MaxActiveConns:   this.MaxActiveConns,
-		ConnMaxIdleTime:  tools.AutoTimeDuration(this.ConnMaxIdleTime, time.Second),
-		ConnMaxLifetime:  tools.AutoTimeDuration(this.ConnMaxLifetime, time.Second),
+		Addrs:            r.Addrs,
+		ClientName:       r.ClientName,
+		Username:         r.User,
+		Password:         r.Password,
+		SentinelUsername: r.SentinelUsername,
+		SentinelPassword: r.SentinelPassword,
+		MasterName:       r.MasterName,
+		DisableIdentity:  r.DisableIdentity,
+		IdentitySuffix:   r.IdentitySuffix,
+		PoolSize:         r.PoolSize,
+		PoolTimeout:      tools.AutoTimeDuration(r.PoolTimeout, time.Second),
+		MinIdleConns:     r.MinIdleConns,
+		MaxIdleConns:     r.MaxIdleConns,
+		MaxActiveConns:   r.MaxActiveConns,
+		ConnMaxIdleTime:  tools.AutoTimeDuration(r.ConnMaxIdleTime, time.Second),
+		ConnMaxLifetime:  tools.AutoTimeDuration(r.ConnMaxLifetime, time.Second),
 		OnConnect: func(ctx context.Context, cn *redis.Conn) error {
 			var err error
-			if this.EnableAuthOnConnect || this.OnConnect {
-				auth := cn.Auth(ctx, this.Password)
+			if r.EnableAuthOnConnect || r.OnConnect {
+				auth := cn.Auth(ctx, r.Password)
 				result, _ := auth.Result()
 				ulogs.Log("redis 二次认证", "结果", result, auth.Err())
 				if err = auth.Err(); err != nil {
 					return err
 				}
 			}
-			if this.EnableSetDbAfterConnect {
+			if r.EnableSetDbAfterConnect {
 				// 在连接的时候，设置库
-				return cn.Select(ctx, this.Db).Err()
+				return cn.Select(ctx, r.Db).Err()
 			}
 			return nil
 		},
 	}
 
-	if this.EnableSetDbBeforeConnect {
-		c.DB = this.Db
+	if r.MaintNotificationsConfig != nil {
+		c.MaintNotificationsConfig = &maintnotifications.Config{
+			Mode:         r.MaintNotificationsConfig.Mode,
+			EndpointType: r.MaintNotificationsConfig.EndpointType,
+		}
 	}
-	ulogs.Log("redis连接参数", this.Addrs, "库编号", this.Db, "二次认证", this.OnConnect || this.EnableAuthOnConnect, "set lib", this.DisableIndentity)
+
+	if r.EnableSetDbBeforeConnect {
+		c.DB = r.Db
+	}
+	ulogs.Log("redis连接参数", r.Addrs, "库编号", r.Db, "二次认证", r.OnConnect || r.EnableAuthOnConnect, "set lib", r.DisableIdentity)
 	rdb := redis.NewUniversalClient(&c)
-	if this.EnableCheckOnInit {
+	if r.EnableCheckOnInit {
 		status := rdb.Ping(context.Background())
 		err := status.Err()
 		if err != nil {
 			return nil, fmt.Errorf("redis指令【%s】执行失败：%s", status.String(), err.Error())
 		}
 	}
-	ulogs.Log("redis连接成功", this.Addrs, "库编号", this.Db, "二次认证", this.OnConnect || this.EnableAuthOnConnect)
+	ulogs.Log("redis连接成功", r.Addrs, "库编号", r.Db, "二次认证", r.OnConnect || r.EnableAuthOnConnect)
 	return rdb, nil
 }
