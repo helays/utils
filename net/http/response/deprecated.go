e33841bcb449639661f833e5698215a901332d33
helei <helei@itestor.com>
2025-12-09 15:10:55 +0800

    SetReturnData 特殊处理code < 100的
    Signed-off-by: helei <helei@itestor.com>

---
+++ net/http/response/deprecated.go
@@ -0,0 +1,143 @@
+package response
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/helays/utils/v2/close/osClose"
+	"github.com/helays/utils/v2/logger/ulogs"
+	"github.com/helays/utils/v2/net/http/httpkit"
+	mime2 "github.com/helays/utils/v2/net/http/mime"
+)
+
+// Play 公共函数文件
+// Deprecated: As of utils v1.1.0, this value is simply [router.Play].
+func Play(path string, w http.ResponseWriter, r *http.Request, args ...any) {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0644)
+	defer osClose.CloseFile(f)
+	if err != nil {
+		ulogs.Error("文件不存在", path)
+		http.NotFound(w, r)
+		return
+	}
+	ranges := int64(0)
+	rangeEnd := int(0)
+	rangeSwap := strings.TrimSpace(r.Header.Get("Range"))
+	if rangeSwap != "" {
+		rangeSwap = rangeSwap[6:]
+		rangListSwap := strings.Split(rangeSwap, "-")
+		if len(rangeSwap) >= 1 {
+			if num, err := strconv.Atoi(rangListSwap[0]); err == nil {
+				ranges = int64(num)
+			}
+			if len(rangeSwap) > 1 {
+				if num, err := strconv.Atoi(rangListSwap[1]); err == nil {
+					rangeEnd = int(num)
+				}
+			}
+		}
+	}
+
+	var (
+		fileSize int
+		//tmpF     []byte
+	)
+	fType := strings.ToLower(filepath.Ext(path)[1:])
+	fInfo, err := f.Stat()
+	if err != nil {
+		Forbidden(w, "403 Forbidden!")
+		return
+	}
+
+	//if fType=="mp4" {
+	//	tmpF = GetMP4Duration(f)
+	//	fileSize= len(tmpF)
+	//	if rangeSwap!="" && ranges>0 {
+	//		tmpF=tmpF[ranges:]
+	//	}
+	//}else{
+	//
+	//	if rangeSwap!="" && ranges>0 {
+	//		_, _ = f.Seek(ranges, 0)
+	//	}
+	//	fileSize=int(fInfo.Size())
+	//}
+	//GetMP4Duration(f)
+	if rangeSwap != "" && ranges > 0 {
+		_, _ = f.Seek(ranges, 0)
+	}
+	fileSize = int(fInfo.Size())
+	totalSize := fileSize
+	if rangeSwap != "" && rangeEnd > 0 {
+		totalSize = rangeEnd
+	}
+	total := strconv.Itoa(fileSize)
+	m := mime2.MimeMap[fType]
+	if m == "" {
+		m = "text/html;charset=utf-8"
+	}
+	w.Header().Set("Content-Type", m)
+	w.Header().Set("Content-Length", strconv.Itoa(totalSize-int(ranges)))
+	w.Header().Set("Last-Modified", fInfo.ModTime().Format(time.RFC822))
+	w.Header().Set("Accept-Ranges", "bytes")
+	w.Header().Set("Connection", "close")
+	w.Header().Set("Etag", `W/"`+strconv.FormatInt(fInfo.ModTime().Unix(), 16)+`-`+strconv.FormatInt(fInfo.Size(), 16)+`"`)
+	if len(args) > 0 {
+		if args[0] == "downloader" {
+			w.Header().Del("Accept-Ranges")
+			httpkit.SetDisposition(w, filepath.Base(path))
+		}
+	}
+	if rangeSwap != "" {
+		w.Header().Set("Content-Range", "bytes "+strconv.Itoa(int(ranges))+"-"+strconv.Itoa(totalSize-1)+"/"+total)
+		w.WriteHeader(206)
+	} else {
+		w.WriteHeader(200)
+	}
+
+	//if fType == "mp4" {
+	//	_byt, _ := io.ReadAll(f)
+	//	_, _ = w.Write(_byt)
+	//	return
+	//}
+	_, _ = io.Copy(w, f)
+}
+
+// SetReturn 设置 返回函数Play
+// Deprecated: 请使用 SetReturnData
+func SetReturn(w http.ResponseWriter, code int, msg ...any) {
+	RespJson(w)
+	if len(msg) < 1 {
+		if code == 0 {
+			msg = []any{"成功"}
+		} else {
+			msg = []any{"失败"}
+		}
+	}
+
+	_ = json.NewEncoder(w).Encode(resp{Code: code, Msg: msg[0]})
+}
+
+// SetReturnCode 设置返回函数
+// code值异常，会记录日志
+// Deprecated: 弃用,请使用 SetReturnData
+func SetReturnCode(w http.ResponseWriter, r *http.Request, code int, msg any, data ...any) {
+	if code != 0 && code != 200 && code != 404 {
+		ReqError(r, code, msg)
+	}
+	if _, ok := msg.(error); ok {
+		if len(data) > 0 && reflect.TypeOf(data[0]).String() == "bool" && !data[0].(bool) {
+			msg = "系统处理失败"
+		} else {
+			msg = msg.(error).Error()
+		}
+	}
+	SetReturnData(w, code, msg, data...)
+}
