4d3659d42e0ded396bb23db92e574e8430ae8839
helei <helei@itestor.com>
2025-12-11 18:14:08 +0800

    新增一个泛型版本的通用 list 2 tree 通过栈方式实现的函数
    Signed-off-by: helei <helei@itestor.com>

---
+++ tools/treeconv/listotree.go
@@ -0,0 +1,81 @@
+package treeconv
+
+type ListItem[T comparable, S TreeNode[S]] interface {
+	GetID() T
+	GetParentID() T
+	ToTreeNode(level int, parentNode ...S) S
+}
+
+type TreeNode[S any] interface {
+	PrepareChildren(n int)
+	AddChild(Node S)
+	GetLastChild() S
+}
+
+func ListToTree[L ListItem[T, S], T comparable, S TreeNode[S]](src []L) []S {
+	var (
+		result []S
+		n      = len(src)
+	)
+	if n == 0 {
+		return result
+	}
+	// 构建映射关系
+	children := make(map[T][]T, n)
+	idToSrc := make(map[T]L, n)
+	for _, item := range src {
+		parentID := item.GetParentID()
+		id := item.GetID()
+		children[parentID] = append(children[parentID], id)
+		idToSrc[id] = item
+	}
+	//对每个根节点分别构建数
+	var zeroID T
+	for _, rootId := range children[zeroID] {
+		level := 1
+		rootData := idToSrc[rootId]
+		rootNode := rootData.ToTreeNode(level)
+		// 使用栈进行深度优先遍历
+		type stackItem struct {
+			node  S
+			id    T
+			level int
+		}
+		stack := []stackItem{{node: rootNode, id: rootId, level: level}}
+		for len(stack) > 0 {
+			// 弹出栈顶
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			// 获取子节点 ID
+			childIds := children[top.id]
+			if len(childIds) == 0 {
+				continue
+			}
+			// 初始化子节点切片
+			top.node.PrepareChildren(len(childIds))
+			// 遍历子节点
+			for _, childId := range childIds {
+				childData := idToSrc[childId]
+				// 创建子节点
+				childNode := childData.ToTreeNode(top.level+1, top.node)
+				// 添加父节点
+				top.node.AddChild(childNode)
+				// 由于上面的SetChildren 里面会解引用，所以这里不能直接用childNode
+				lastChild := top.node.GetLastChild()
+				// 确保 GetLastChild() 不为 nil
+				if lastChild == nil {
+					continue
+				}
+				stack = append(stack, stackItem{
+					node:  lastChild,
+					id:    childId,
+					level: top.level + 1,
+				})
+			}
+		}
+
+		result = append(result, rootNode)
+	}
+
+	return result
+}
