839a4a58f8a29a6256acb60092286c067d56e7fe
helei <helei@itestor.com>
2026-01-05 15:03:53 +0800

    新增一个通用的 DiffRangeable 比较函数。 可比较两个可遍历数组中，差异元素。
    Signed-off-by: helei <helei@itestor.com>

--- tools/compare.go
+++ tools/compare.go
@@ -105,3 +105,52 @@ func IsZero[T comparable](v T) bool {
 	var zero T
 	return v == zero
 }
+
+// Rangeable 定义可用于range的接口
+// Idx: 集合的键类型（数组索引、map键等）
+// V: 元素值类型
+// C: 用于比较的键类型
+// noinspection all
+type Rangeable[Idx comparable, V any, C comparable] interface {
+	Range(func(Idx, V))
+	Len() int
+	ExtractKey(V) C
+}
+
+// DiffRangeable 比较两个Rangeable
+// 返回两个Rangeable的差异
+// src: 源数据
+// dst: 目标数据
+// 返回值:
+// inSrc: 目标数据中不存在的元素
+// common: 两个Rangeable中都存在的元素
+// inDst: 源数据中不存在的元素
+// noinspection all
+func DiffRangeable[Idx comparable, V any, C comparable](src, dst Rangeable[Idx, V, C]) (inSrc, common, inDst []V) {
+	elementMap := make(map[C]int8, src.Len()+dst.Len())
+	// 遍历源数据,标记成1
+	src.Range(func(_ Idx, v V) {
+		elementMap[src.ExtractKey(v)] = 1
+	})
+	// 遍历目标数据
+	dst.Range(func(_ Idx, v V) {
+		val := dst.ExtractKey(v)
+		if status, exists := elementMap[val]; exists {
+			// 原数组中存在的
+			if status == 1 {
+				elementMap[val] = 3 // 标记共同存在
+				common = append(common, v)
+			}
+		} else {
+			elementMap[val] = 2
+			inDst = append(inDst, v)
+		}
+	})
+	// 收集只在源数组中存在的元素
+	src.Range(func(_ Idx, v V) {
+		if status := elementMap[src.ExtractKey(v)]; status == 1 {
+			inSrc = append(inSrc, v)
+		}
+	})
+	return
+}
